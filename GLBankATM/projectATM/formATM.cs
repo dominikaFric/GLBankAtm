86d56b49378f0094d0c837d3494f3627a7eed699
domiNik4 <dominika.fric@akademiasovy.sk>
2017-05-09 08:16:42 +0200

    initial push

---
+++ GLBankATM/projectATM/formATM.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projectATM
+{
+    public partial class formATM : Form
+    {
+
+        private long id;
+
+        public formATM(long id)
+        {
+            InitializeComponent();
+            this.id = id;
+            
+        }
+
+        private void formATM_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
